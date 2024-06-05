2adc3e1a27f1692d2e97699f22ee2b3adb3eb98f
Hoang-Nguyen-Huy <hoangmegamind1302@gmail.com>
2024-06-06 00:49:47 +0700

    feat: connect with mysql

---
+++ src/entities/tasks.entity.ts
@@ -0,0 +1,19 @@
+import { Status } from "src/enum/status.enum";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Tasks {
+    @PrimaryGeneratedColumn("uuid")
+    taskId: string;
+
+    @Column()
+    title: string;
+
+    @Column()
+    description: string;
+
+    @Column({
+        default: Status.Incompleted,
+    })
+    status: Status;
+}
---
+++ src/enum/status.enum.ts
@@ -0,0 +1,4 @@
+export enum Status {
+    Completed = 'completed',
+    Incompleted = 'incompleted'
+}
